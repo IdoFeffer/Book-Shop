e365f512c1dfb91713a5c8fe18e53a25a91ae007
Ido Feffer <idoppr1@gmail.com>
2025-03-20 16:40:03 +0200

    Add remove or update book

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,23 +1,40 @@
-'use strict'
+"use strict"
 
-function onInit(){
-    renderBooks()
+function onInit() {
+  renderBooks()
 }
 
-function renderBooks(){
-    const books = getBooks()
-    var elTableBody = document.querySelector('.book-list')
+function renderBooks() {
+  const books = getBooks()
+  const elTableBody = document.querySelector(".book-list")
 
-    elTableBody.innerHtml = ''
+  var strHTMLs = books.map((book) => {
+    return `
+          <tr>
+              <td>${book.title}</td>
+              <td>${book.price}</td>
+              <td>
+                  <button class="read" onclick="onRead(${book.id})">Read</button>
+                  <button class="update" onclick="onUpdateBook(${book.id})">Update</button>
+                  <button class="delete" onclick="onRemoveBook(${book.id},event)">Delete</button>
+              </td>
+          </tr>`
+  })
+  elTableBody.innerHTML = strHTMLs.join("")
+}
 
-    books.forEach(book => {
-        elTableBody.innerHTML += `
-        <tr>
-        <td>${book.title}</td>
-        <td>${book.price}</td>
-        <td>
-            <button onclick='onRead'(${book.id})>
-            <button onclick='onUpdate'(${book.id})>
-            <button onclick='onDelete'(${book.id})>`
-    });
+function onRemoveBook(bookId, ev) {
+  ev.stopPropagation()
+  // Update the Model:
+  removeBook(bookId)
+  // Update the Dom:
+  renderBooks()
+}
+
+function onUpdateBook(bookId) {
+  const newPrice = +prompt("Enter new price:")
+  if (!newPrice || newPrice === 0) return
+
+  updatePrice(bookId, newPrice)
+  renderBooks()
 }
--- js/services/book.service.js
+++ js/services/book.service.js
@@ -1,11 +1,25 @@
 "use strict"
 
-const books = [
+// var gBooks
+
+const gBooks = [
   { id: 1, title: "The Adventure of Lori Ipsi", price: 120 },
   { id: 2, title: "World Atlas", price: 300 },
-  { id: 3, title: "Zorba The Greek", price: 87 }
+  { id: 3, title: "Zorba The Greek", price: 87 },
 ]
 
 function getBooks() {
-  return books
+  return gBooks
 }
+
+function removeBook(bookId) {
+  const bookIdx = gBooks.findIndex((book) => book.id === bookId)
+  if (bookIdx !== -1) gBooks.splice(bookIdx, 1)
+}
+
+function updatePrice(bookId, newPrice) {
+  const book = gBooks.find(book => book.id === bookId);
+  if (!book) return;
+  book.price = newPrice; 
+}
+
